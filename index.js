fa2ef8b5ef4cfba6ead5a648e312eb1bae56df11
vikmaamaa <maamaavictor@yahoo.com>
2023-09-08 02:19:02 +0100

    feat: done

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express')
+
+const morgan = require("morgan")
+
+const cors = require("cors")
+
+require("dotenv").config()
+
+
+//handles uncaught errors
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION! Shutting Down....')
+    process.exit(1);
+})
+
+
+const app = express()
+
+
+
+app.use(morgan("dev"))
+
+app.use(express.json({ limit: '10kb' }));
+app.use(cors()) 
+
+ app.use('/', async(req, res)=>{
+        const { slack_name, track } = req.query;
+
+    // Check if required parameters are provided
+    if (!slack_name || !track) {
+        return res.status(400).json({ error: 'slack_name and track are required query parameters' });
+    }
+
+    // Get the current day
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const currentDate = new Date();
+    const currentDay = daysOfWeek[currentDate.getUTCDay()];
+
+    // Get the current UTC time
+    const currentUTC = new Date().toISOString();
+
+    // GitHub repository information
+    const githubUsername = 'VikMaamaa';
+    const githubRepo = 'HNGX-Task-1';
+    const githubFile = 'index.js';
+    const githubFileURL = `https://github.com/${githubUsername}/${githubRepo}/${githubFile}`;
+    const githubRepoURL = `https://github.com/${githubUsername}/${githubRepo}`;
+
+    const response = {
+        slack_name,
+        current_day: currentDay,
+        utc_time: currentUTC,
+        track,
+        github_file_url: githubFileURL,
+        github_repo_url: githubRepoURL,
+        status_code: 200,
+    };
+
+    res.status(200).json(response);
+ });
+
+
+//connection port 
+const port = process.env.PORT || 8000
+
+ app.listen(port, ()=>console.log(`Server is running on port ${port}`))
+
+
